13295a5b7b720378d8ee794df662d6033b8fa7d9
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-08-20 14:21:18 +0300

    feat: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,27 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      },
+      {
+        find: '@public',
+        replacement: fileURLToPath(new URL('./public', import.meta.url))
+      },
+      {
+        find: '@components',
+        replacement: fileURLToPath(new URL('./src/components', import.meta.url))
+      },
+      {
+        find: '@stores',
+        replacement: fileURLToPath(new URL('./src/stores', import.meta.url))
+      }
+    ]
+  }
 })
