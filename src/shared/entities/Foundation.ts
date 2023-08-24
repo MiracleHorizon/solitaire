c65086d40fe91b8a830cf5e6423e54e92e8cf123
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-08-24 06:08:35 +0300

    feat(entities): add new methods/field to Foundation entity

--- src/shared/entities/Foundation.ts
+++ src/shared/entities/Foundation.ts
@@ -1,12 +1,30 @@
 import { Card } from './Card.ts'
+import { Suit } from '@app-types/card'
 import type { IFoundation } from '@app-types/Foundation.ts'
 
 export class Foundation implements IFoundation {
+  public suit: Suit | null = null
   public cards: Card[] = []
 
   constructor(public readonly id: number) {}
 
+  public get isEmpty(): boolean {
+    return this.cards.length === 0
+  }
+
+  public setSuit(suit: Suit): void {
+    this.suit = suit
+  }
+
+  public removeSuit(): void {
+    this.suit = null
+  }
+
   public addCard(card: Card): void {
+    if (!this.suit) {
+      this.setSuit(card.suit)
+    }
+
     card.setFoundation(this.id)
     card.setColumn(null)
     this.cards.push(card)
@@ -14,5 +32,13 @@ export class Foundation implements IFoundation {
 
   public removeCard(cardId: string): void {
     this.cards = this.cards.filter(card => card.id !== cardId)
+
+    if (this.cards.length === 0) {
+      this.removeSuit()
+    }
+  }
+
+  public tryToGetLastCard(): Card | null {
+    return this.cards[this.cards.length - 1] ?? null
   }
 }
