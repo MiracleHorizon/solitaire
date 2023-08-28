b48eb1cd2e59731d0c4b79c2be86904bdff3a9a8
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-08-28 12:19:55 +0300

    chore(Board component): add paddings

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -18,6 +18,19 @@
   align-items: center;
   justify-content: flex-start;
   flex-direction: column;
+  padding-top: 24px;
+  padding-left: 18px;
+  padding-right: 18px;
   background: url('@static/images/board_bg.png') $board-green;
+
+  @media screen and (max-width: $tablet) {
+    padding-left: 14px;
+    padding-right: 14px;
+  }
+
+  @media screen and (max-width: $mobileLg) {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
 }
 </style>
