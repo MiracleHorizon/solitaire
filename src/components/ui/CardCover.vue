9c7b682b8ab2f0202c6ef5ef7cd7c02b0a92487d
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-08-28 12:21:37 +0300

    feat(ui): add Card and CardCover components

---
+++ src/components/ui/CardCover.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import cardCover from '@images/cards/card_cover.png'
+</script>
+
+<template>
+  <div :class="$style.root">
+    <img :src="cardCover" alt="Card cover" />
+  </div>
+</template>
+
+<style module lang="scss">
+@import '@styles/mixins';
+
+.root {
+  z-index: 0;
+  opacity: 0.4;
+
+  img {
+    @include no-select;
+    pointer-events: none;
+  }
+}
+</style>
