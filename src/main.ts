059d860e638339a2a3254ecbff4046b5a42bcad6
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-08-30 16:59:36 +0300

    feat: configure vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue'
 import '@styles/main.scss'
 
-const pinia = createPinia()
 const app = createApp(App)
+const pinia = createPinia()
+const router = createRouter({
+  history: createWebHistory(),
+  routes: []
+})
 
 app.use(pinia)
+app.use(router)
 app.mount('#app')
