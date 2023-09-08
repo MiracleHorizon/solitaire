e7a4ab55431da510d65943e27979d702fa1fd47e
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-09-08 17:43:59 +0300

    feat: add generateCard helper

---
+++ src/shared/helpers/generateCard.ts
@@ -0,0 +1,31 @@
+import { v4 } from 'uuid'
+
+import { Card } from '@entities/Card.ts'
+import { Color, Rank, Suit } from '@app-types/card'
+
+interface Arguments extends DynamicFields {
+  rank?: Rank
+  suit?: Suit
+}
+
+interface DynamicFields {
+  wasDealt?: boolean
+  isFlipped?: boolean
+  column?: number | null
+  base?: number | null
+}
+
+export const generateCard = (args?: Arguments): Card => {
+  return new Card({
+    id: v4(),
+    wasDealt: false,
+    isFlipped: false,
+    base: null,
+    column: null,
+    rank: Rank.ACE,
+    suit: Suit.SPADES,
+    color: Color.RED,
+    image: 'spades ace image',
+    ...args
+  })
+}
