2e82a5684b4f721f23e163249020eeb81ec51bd7
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-09-08 16:45:11 +0300

    chore: double quotes in jest config change to single

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,12 +4,12 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'ts', 'vue'],
   moduleNameMapper: {
-    "^@stores(.*)$": "<rootDir>/src/stores$1",
-    "^@shared(.*)$": "<rootDir>/src/shared$1",
-    "^@utils(.*)$": "<rootDir>/src/shared/utils$1",
-    "^@helpers(.*)$": "<rootDir>/src/shared/helpers$1",
-    "^@entities(.*)$": "<rootDir>/src/shared/entities$1",
-    "^@app-types(.*)$": "<rootDir>/src/shared/types$1",
+    '^@stores(.*)$': '<rootDir>/src/stores$1',
+    '^@shared(.*)$': '<rootDir>/src/shared$1',
+    '^@utils(.*)$': '<rootDir>/src/shared/utils$1',
+    '^@helpers(.*)$': '<rootDir>/src/shared/helpers$1',
+    '^@entities(.*)$': '<rootDir>/src/shared/entities$1',
+    '^@app-types(.*)$': '<rootDir>/src/shared/types$1'
   },
   collectCoverage: true
 }
