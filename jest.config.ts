c594a6036ccc6bb1e975ad0241cefe4ac2c7527a
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-09-15 02:21:39 +0300

    chore(configs): update formats

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+import type { Config } from 'jest'
+
+const jestConfig: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'ts', 'vue'],
@@ -7,7 +8,7 @@ module.exports = {
   resetMocks: false,
   cache: true,
   setupFiles: ['jest-localstorage-mock'],
-  setupFilesAfterEnv: ['./src/__setup__/configs/jest.setup.cjs'],
+  setupFilesAfterEnv: ['./src/__setup__/configs/jest.setup.ts'],
   moduleNameMapper: {
     '^__setup__(.*)$': '<rootDir>/src/__setup__$1',
     '^@stores(.*)$': '<rootDir>/src/stores$1',
@@ -19,3 +20,5 @@ module.exports = {
     '.+\\.(png|jpg|jpeg|mp3)$': 'identity-obj-proxy'
   }
 }
+
+export default jestConfig
