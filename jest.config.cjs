7416f6bcb32669f4c24301f9d20cf3041626c631
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-09-14 17:51:31 +0300

    tests(config): add setupFilesAfterEnv directory

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
   resetMocks: false,
   cache: true,
   setupFiles: ['jest-localstorage-mock'],
+  setupFilesAfterEnv: ['./src/__setup__/configs/jest.setup.cjs'],
   moduleNameMapper: {
     '^__setup__(.*)$': '<rootDir>/src/__setup__$1',
     '^@stores(.*)$': '<rootDir>/src/stores$1',
