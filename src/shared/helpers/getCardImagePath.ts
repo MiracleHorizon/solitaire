3008d29977dd97b1ea6041f92d51a4c964dc54a6
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-11-01 00:12:41 +0300

    chore(tests): add tests for the getCardImagePath helper

--- src/shared/helpers/getCardImagePath.ts
+++ src/shared/helpers/getCardImagePath.ts
@@ -1,13 +1,19 @@
-import { getBasicCardImagePath } from '@helpers/getBasicCardImagePath.ts'
+import { Rank, type Suit } from '@app-types/card'
 import type { GameCardStyle } from '@stores/interface.ts'
-import type { Rank, Suit } from '@app-types/card'
+
+export const PUBLIC_IMAGES_PATH = '/images/cards'
 
 export const getCardImagePath = (
   suit: Suit,
   rank: Rank,
-  style: GameCardStyle
+  styleVariant: GameCardStyle
 ): string => {
-  const basePath = getBasicCardImagePath(suit, rank)
+  const rankPathEntry = Object.entries(Rank)
+    .filter(([key]) => isNaN(Number(key)))
+    .map(([key, value]) => [key.toLowerCase(), value])
+    .find(([, value]) => value === rank)
 
-  return `${basePath}_v${style}.png`
+  const basePath = `${PUBLIC_IMAGES_PATH}/${suit}/${suit}_${rankPathEntry![0]}`
+
+  return `${basePath}_v${styleVariant}.png`.toLowerCase()
 }
